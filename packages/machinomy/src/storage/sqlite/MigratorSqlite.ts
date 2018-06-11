c1a64cabeed34e7b3431d5635ff7bd03a9c5bb5a
Andrei Riaskov <ryaskov.work@gmail.com>
2018-06-11 14:11:33 +0300

    Add migrator for SQLite.

---
+++ packages/machinomy/src/storage/sqlite/MigratorSqlite.ts
@@ -0,0 +1,86 @@
+import IMigrator from '../IMigrator'
+import EngineSqlite from './EngineSqlite'
+import * as fs from 'fs'
+const DBMigrate = require('db-migrate')
+const dbmigrate = DBMigrate.getInstance(true, { cwd: '../../../../migrations/' })
+const LENGTH_OF_MIGRATION_NAME = 14
+
+export default class MigratorSqlite implements IMigrator {
+  engine: EngineSqlite
+
+  constructor (engine: EngineSqlite) {
+    this.engine = engine
+  }
+
+  isLatest (): Promise<boolean> {
+    return new Promise(async (resolve) => {
+      const commonIndex: number = await this.getCommonIndex()
+      if (commonIndex === -1) {
+        resolve(true)
+      } else {
+        resolve(false)
+      }
+    })
+  }
+
+  sync (n?: string): Promise<void> {
+    return new Promise(async (resolve) => {
+      if (n !== undefined) {
+        if (n.length === LENGTH_OF_MIGRATION_NAME) {
+          dbmigrate.sync(n)
+        } else {
+          console.error('DB migration name must have ' + LENGTH_OF_MIGRATION_NAME + ' chars. But got ' + n.length)
+        }
+      } else {
+        dbmigrate.sync()
+      }
+      resolve()
+    })
+  }
+
+  private async getCommonIndex (): Promise<number> {
+    const migrationsInDB = await this.retrieveUpMigrationList()
+    const migrationsInFolder = await this.retrieveInFolderMigrationList()
+    let commonIndex = -1
+    for (let i = 0; i < migrationsInFolder.length; i++) {
+      if (migrationsInDB[i] !== migrationsInFolder[i]) {
+        commonIndex = i
+        break
+      }
+    }
+
+    return Promise.resolve(commonIndex)
+  }
+
+  private retrieveUpMigrationList (): Promise<string[]> {
+    return new Promise((resolve) => {
+      this.engine.exec((client: any) => client.query(
+        'SELECT name FROM migrations ORDER BY name ASC'
+      )).then((res: any) => {
+        const names: string[] = res.rows
+        let result: string[] = []
+        for (let migrationName in names) {
+          result.push(migrationName.substring(1))
+        }
+        resolve(result)
+      })
+    })
+  }
+
+  private retrieveInFolderMigrationList (): Promise<string[]> {
+    return new Promise(async (resolve) => {
+      let result: string[] = []
+      const listOfFiles: string[] = fs.readdirSync('../../../../migrations/')
+      for (let filename in listOfFiles) {
+        const isDir = fs.statSync(filename).isDirectory()
+        if (!isDir) {
+          filename = filename.slice(0, -3)
+          result.push(filename)
+          console.log('debug::DB migration file: ' + filename)
+        }
+      }
+      result = result.sort((a: string, b: string) => a.localeCompare(b))
+      resolve(result)
+    })
+  }
+}
